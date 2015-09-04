6ec5faca183aab9a0ad23b16ad50c4dbb9512c2c
Kate <queenliestme@mail.ru>
2015-09-04 03:50:59 +0300

    No longer store incident vertices in the Node class; Add only one incident vertice at time; Incident vertices must be of the class Node.

--- graph.py
+++ graph.py
@@ -1,32 +1,46 @@
 from node import Node
 
 class Graph(object):
-	def __init__(self):
-		self.graph = list()
+    def __init__(self):
+        self.graph = dict()
 
-	def add_node(self, new_node): 
-		if isinstance(new_node, Node):
-			self.graph.append(new_node)
-		else:
-			raise NameError("Not a node!")
-	
-	def print_graph(self):
-		print self.graph
+    def add_node(self, new_node): 
+        if isinstance(new_node, Node):
+            self.graph.setdefault(new_node, list())
+        else:
+            raise NameError("Not a node!")
 
-	def delete_node(self):
-		pass
-	def find_node(self):
-		pass
-	def walk_graph(self, func):
-		pass
-	def show_graph(self):
-		pass
-	def show_nodes(self):
-		pass
-	def show_edeges(self, node):
-		pass
+    def add_incident_vertice(self, node_name, incident_vertice):
+        if isinstance(node_name, Node) and isinstance(incident_vertice, Node):
+            self.graph[node_name].append(incident_vertice)
+        else:
+            raise NameError("Not a node!")
+    
+    def print_graph(self):
+        for node in self.graph:
+            print node + ":" + str(self.graph[node]) 
+
+    def delete_node(self):
+        pass
+    def find_node(self):
+        pass
+    def walk_graph(self, func):
+        pass
+    def show_graph(self):
+        pass
+    def show_nodes(self):
+        pass
+    def show_edeges(self, node):
+        pass
 
 my_node = Node("I_am_node_and_I_love_u")
+my_node1 = Node("I_am_node_and_I_love_u")
+
 my_graph = Graph()
+
 my_graph.add_node(my_node)
+my_graph.add_node(my_node1)
+
+my_graph.add_incident_vertice(my_node, my_node)
+#my_graph.graph["I_am_node_and_I_love_u"]
 my_graph.print_graph()
--- node.py
+++ node.py
@@ -1,29 +1,16 @@
 
 class Node(object):
-	def __init__(self, name):
-		self.node_name = name
-		self.incident_vertices= ()
+    def __init__(self, name):
+        self.node_name = name
 
-	def __add__(self, other):
-		return self.node_name + other
+    def __add__(self, other):
+        return self.node_name + other
 
-	def __repr__(self):
-		return self.node_name
+    def __repr__(self):
+        return self.node_name
 
-	def __hash__(self):
-		return hash(self.node_name)
+    def __hash__(self):
+        return hash(self.node_name)
 
-	def __eq__(self, other_node):
-		return isinstance(other_node, Node) and self.node_name == other_node.node_name
-
-	def add_verticies(self, incident_vertices):
-		for vertice in incident_vertices:
-			self.incident_vertices.append(vertice)
-
-
-
-
-
-
-
-				
+    def __eq__(self, other_node):
+        return isinstance(other_node, Node) and self.node_name == other_node.node_name
